4ef5e0b64a695f3bf480b836b32e7bc39585ef39
cool <c00lkidd_lol@outlook.com>
2025-05-05 17:08:04 -0400

    Create script.js

---
+++ cdn/script.js
@@ -0,0 +1,12 @@
+alert("Welcome! This is UTILLY, an completely free Discord bot that helps configure your server, just lay your hands back and watch everything be done for you! And, no need for an whip, we work hard by our self!")
+window.addEventListener('load', () => {
+      const elem = document.documentElement;
+
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+  }
+});
