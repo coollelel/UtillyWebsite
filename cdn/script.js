d31063f5734d3c501aca1a29d55f7d9ddb184cfa
cool <c00lkidd_lol@outlook.com>
2025-05-05 17:27:09 -0400

    Update script.js

--- cdn/script.js
+++ cdn/script.js
@@ -2,10 +2,19 @@
   const alertBox = document.getElementById('alertBox');
   const customAlert = document.getElementById('customAlert');
   const ding = document.getElementById('dingSound');
+  const closeBtn = document.getElementById('closeButton');
+  window.focus()
   overlay.addEventListener('click', () => {
     overlay.style.display = 'none';
     customAlert.style.display = 'flex';
     ding.play();
+  });
+  closeBtn.addEventListener('click', () => {
+    alertBox.classList.add('fade-out');
+    setTimeout(() => {
+      customAlert.style.display = 'none';
+      alertBox.classList.remove('fade-out');
+    }, 400);
   });
   let isDragging = false, offsetX = 0, offsetY = 0;
   alertBox.addEventListener("mousedown", (e) => {
